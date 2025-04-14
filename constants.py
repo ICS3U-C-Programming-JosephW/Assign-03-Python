1e65830843fc26799d81ee42ab37a030f0025ef2
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-04-14 17:41:51 -0400

    Made changes to template.py (letter_classifier.py) and added constants.py.

---
+++ constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: Apr. 14, 2025
+# This module contains constants.
+
+ENGLISH_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+                   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+                   'u', 'v', 'w', 'x', 'y', 'z']
+
+PHONETIC_SYMBOLS = ["/ˈeɪ/", "/ˈbiː/", "/ˈsiː/", "/ˈdiː/", 
+                    "/ˈiː/", "/ˈɛf/", "/ˈdʒiː/", "/eɪtʃ/", 
+                    "/ˈaɪ/", "/ˈdʒeɪ/", "/ˈkeɪ/", "/ˈɛl/", 
+                    "/ˈɛm/", "/ˈɛn/", "/ˈoʊ/", "/ˈpiː/", 
+                    "/ˈkjuː/", "/ˈɑːr/", "/ˈɛs/", "/ˈtiː/", 
+                    "/ˈjuː/", "/ˈviː/", "/ˈdʌb.əlˈjuː/", 
+                    "/ˈɛks/", "/ˈwaɪ/", "/ˈziː/ or /ˈzɛd/"]
+
+EXAMPLE_WORDS = ["ate", "bean", "cow", "deed", "ever", "friend", 
+                 "gene", "house", "ice", "jam", "kangaroo", "lid", 
+                 "make", "new", "one", "pen", "queue", "rain", "stumble", 
+                 "tea", "under", "vary", "well", "xylophone", "yarn", "zebra"]
---
+++ letter_classifier.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: Apr. 14, 2025
+"""
+This program determines whether a letter of the English
+alphabet is a consonant, vowel, or semivowel. It also
+allows the user to find extra information about a particular
+letter by display phonetic symbols and examples if desired.
+"""
+
+#
+import constants
+
+# Define the main function.
+def main():
+    pass
+
+
+# Check if the special name of the file is __main__.
+if __name__ == "__main__":
+    # Run the main function if so.
+    main()
