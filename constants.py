24cdffc5530b8e851516884b23646081dfe4eac6
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-04-14 18:43:56 -0400

    Modified letter_classifier.py. Added repeated error checks; unfinished.

--- constants.py
+++ constants.py
@@ -3,10 +3,10 @@
 # Date: Apr. 14, 2025
 # This module contains constants.
 
+# For input error detections.
 ENGLISH_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
                    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
                    'u', 'v', 'w', 'x', 'y', 'z']
-
 PHONETIC_SYMBOLS = ["/ˈeɪ/", "/ˈbiː/", "/ˈsiː/", "/ˈdiː/", 
                     "/ˈiː/", "/ˈɛf/", "/ˈdʒiː/", "/eɪtʃ/", 
                     "/ˈaɪ/", "/ˈdʒeɪ/", "/ˈkeɪ/", "/ˈɛl/", 
@@ -14,8 +14,17 @@ PHONETIC_SYMBOLS = ["/ˈeɪ/", "/ˈbiː/", "/ˈsiː/", "/ˈdiː/",
                     "/ˈkjuː/", "/ˈɑːr/", "/ˈɛs/", "/ˈtiː/", 
                     "/ˈjuː/", "/ˈviː/", "/ˈdʌb.əlˈjuː/", 
                     "/ˈɛks/", "/ˈwaɪ/", "/ˈziː/ or /ˈzɛd/"]
-
 EXAMPLE_WORDS = ["ate", "bean", "cow", "deed", "ever", "friend", 
                  "gene", "house", "ice", "jam", "kangaroo", "lid", 
                  "make", "new", "one", "pen", "queue", "rain", "stumble", 
                  "tea", "under", "vary", "well", "xylophone", "yarn", "zebra"]
+
+# For text escape codes.
+LIGHT_GREEN = "\033[1;32m"
+LIGHT_CYAN = "\033[1;36m"
+LIGHT_PURPLE = "\033[1;35m"
+LIGHT_RED = "\033[1;31m"
+DARK_GRAY = "\033[1;30m"
+LIGHT_BLUE = "\033[1;34m"
+LIGHT_YELLOW = "\033[1;33m"
+WHITE = "\033[0m"
--- letter_classifier.py
+++ letter_classifier.py
@@ -1,19 +1,57 @@
 #!/usr/bin/env python3
 # Created By: Joseph Wondimagnehu
 # Date: Apr. 14, 2025
-"""
-This program determines whether a letter of the English
-alphabet is a consonant, vowel, or semivowel. It also
-allows the user to find extra information about a particular
-letter by display phonetic symbols and examples if desired.
-"""
+# This program determines whether a letter of the English
+# alphabet is a consonant, vowel, or semivowel. It also
+# allows the user to find extra information about a particular
+# letter by displaying phonetic symbols and examples if desired.
 
-#
+# Import the constants module to use constants.
 import constants
 
 # Define the main function.
 def main():
-    pass
+    # Construct an infinite loop.
+    while True:
+        # Get the desired letter from the user.
+        user_letter = input("Enter a letter from a-z or A-Z: ")
+
+        # Checks if the lowercase letter entered is a 
+        # letter of the English alphabet.
+        if user_letter.lower() in constants.ENGLISH_LETTERS:
+            # Break the infinite loop.
+            break
+        # Otherwise, they did not enter a valid letter.
+        else:
+            # Display to the user that they must
+            # enter a valid letter of the English alphabet.
+            print(f"\n{user_letter} is not a valid letter of the English alphabet. "
+            "Please enter a letter from a-z or A-Z.\n")
+
+    # Construct an infinite while loop.
+    while True:
+        # Get the desired display type from the user.
+        user_display_type = input("\nEnter a display type for the letter "
+        "(simple = straightforward answer, complex = sophisticated answer): ")
+
+        # Check if simple or complex was the entered display type.
+        if ((user_display_type.lower() == "simple") or (user_display_type.lower() == "complex")):
+            # Break the infinite loop.
+            break
+        # Otherwise, they did not enter a valid display type.
+        else:
+            # Display to the user that they must enter a valid choice.
+            print(f"{user_display_type} is not a valid choice. Please "
+            "enter either simple or complex for the display type.")
+
+                    
+    
+
+
+
+
+
+
 
 
 # Check if the special name of the file is __main__.
